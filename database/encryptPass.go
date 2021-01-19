2ed02d3f96dc389ff3c5c7e14e7f90b6c92220dd
francobottoni <40696735+francobottoni@users.noreply.github.com>
2021-01-18 21:36:31 -0300

    v1.0.1

--- database/encryptPass.go
+++ database/encryptPass.go
@@ -1,9 +1,9 @@
-package middleware
+package database
 
 import "golang.org/x/crypto/bcrypt"
 
 func EncryptPassword(pass string) (string, error) {
 	cost := 8 //Cost is the number of times you will encrypt the password
-	bytes, err := bcrypt.GenerateFromPassword([]bytes(pass), cost)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pass), cost)
 	return string(bytes), err
 }
